5957d7f1388732d975d84473c51e76a312928e02
DavidPyn27 <popayan159@gmail.com>
2018-07-26 11:13:04 -0500

    commitz1

--- Turisapp5/app/src/main/java/com/aprendiz/ragp/turisapp5/fragments/FragmentHoteles.java
+++ Turisapp5/app/src/main/java/com/aprendiz/ragp/turisapp5/fragments/FragmentHoteles.java
@@ -1,13 +1,16 @@
 package com.aprendiz.ragp.turisapp5.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.aprendiz.ragp.turisapp5.R;
+import com.aprendiz.ragp.turisapp5.maps.HotelMaps;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,8 +26,19 @@ public class FragmentHoteles extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_hoteles, container, false);
+        View view = inflater.inflate(R.layout.fragment_fragment_hoteles, container, false);
+
+        FloatingActionButton hot = view.findViewById(R.id.btnHotel);
+        hot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), HotelMaps.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
     }
 
 }
